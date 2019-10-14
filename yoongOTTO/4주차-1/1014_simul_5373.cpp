758212a5a774d4fd422e1149203627af0046f260
devOTTO <yoongotto@gmail.com>
2019-10-15 00:56:26 +0900

    Update 5373

--- yoongOTTO/4주차-1/1014_simul_5373.cpp
+++ yoongOTTO/4주차-1/1014_simul_5373.cpp
@@ -1,6 +1,3 @@
-//
-// Created by 융미 on 2019-10-14.
-//
 
 //5373 큐빙
 #include <iostream>
@@ -11,32 +8,70 @@ using namespace std;
 
 int tc;
 vector<pair<char,char>> vec;
-char fArr[3][3], bArr[3][3], uArr[3][3], lArr[3][3], rArr[3][3], dArr[3][3];
+
+class side {
+public:
+    char arr[3][3];
+public:
+    void right(){
+        char temp[3][3];
+        for(int i = 0; i<3; ++i)
+            for(int j = 0; j<3; ++j)
+                temp[i][j] = arr[i][j];
+
+        arr[0][0] = temp[2][0];
+        arr[0][1] = temp[1][0];
+        arr[0][2] = temp[0][0];
+        arr[1][0] = temp[2][1];
+        arr[1][2] = temp[0][1];
+        arr[2][0] = temp[2][2];
+        arr[2][1] = temp[1][2];
+        arr[2][2] = temp[0][2];
+    }
+    void left(){
+        char temp[3][3];
+        for(int i = 0; i<3; ++i)
+            for(int j = 0; j<3; ++j)
+                temp[i][j] = arr[i][j];
+
+        arr[0][0] = temp[0][2];
+        arr[0][1] = temp[1][2];
+        arr[0][2] = temp[2][2];
+        arr[1][0] = temp[0][1];
+        arr[1][2] = temp[2][1];
+        arr[2][0] = temp[0][0];
+        arr[2][1] = temp[1][0];
+        arr[2][2] = temp[2][0];
+    }
+    void init(char c){
+        for(int i = 0; i<3; ++i)
+            for(int j = 0; j<3; ++j)
+                arr[i][j] = c;
+    }
+
+};
+
+side fArr, bArr, uArr, lArr, rArr, dArr;
 
 void init(){
-
-    for(int i = 0; i<3; ++i){
-        for(int j = 0; j<3; ++j) {
-            fArr[i][j] = 'r';
-            bArr[i][j] = 'o';
-            uArr[i][j] = 'w';
-            lArr[i][j] = 'g';
-            rArr[i][j] = 'b';
-            dArr[i][j] = 'y';
-        }
-    }
+    fArr.init('r');
+    bArr.init('o');
+    uArr.init('w');
+    lArr.init('g');
+    rArr.init('b');
+    dArr.init('y');
 }
-
 
 void LMinus() {
     int j = 0;
     char temp[3];
+    lArr.left();
     for(int i = 0; i<3; ++i){
-        temp[i] = bArr[2-i][j];
-        bArr[2-i][j] = uArr[i][j];
-        uArr[i][j] = fArr[i][j];
-        fArr[i][j] = dArr[i][j];
-        dArr[i][j] = temp[i];
+        temp[i] = bArr.arr[2-i][j];
+        bArr.arr[2-i][j] = uArr.arr[i][j];
+        uArr.arr[i][j] = fArr.arr[i][j];
+        fArr.arr[i][j] = dArr.arr[i][j];
+        dArr.arr[i][j] = temp[i];
     }
 
 }
@@ -44,36 +79,39 @@ void LMinus() {
 void LPlus(){
     int j = 0;
     char temp[3];
+    lArr.right();
     for(int i = 0; i<3; ++i){
-        temp[i] = uArr[i][j];
-        uArr[i][j] = bArr[2-i][j];
-        bArr[2-i][j] = dArr[i][j];
-        dArr[i][j] = fArr[i][j];
-        fArr[i][j] = temp[i];
+        temp[i] = uArr.arr[i][j];
+        uArr.arr[i][j] = bArr.arr[2-i][j];
+        bArr.arr[2-i][j] = dArr.arr[i][j];
+        dArr.arr[i][j] = fArr.arr[i][j];
+        fArr.arr[i][j] = temp[i];
     }
 }
 
 void RMinus(){
     int j = 2;
     char temp[3];
+    rArr.left();
     for(int i = 0; i<3; ++i){
-        temp[i] = uArr[i][j];
-        uArr[i][j] = bArr[2-i][j];
-        bArr[2-i][j] = dArr[i][j];
-        dArr[i][j] = fArr[i][j];
-        fArr[i][j] = temp[i];
+        temp[i] = uArr.arr[i][j];
+        uArr.arr[i][j] = bArr.arr[2-i][j];
+        bArr.arr[2-i][j] = dArr.arr[i][j];
+        dArr.arr[i][j] = fArr.arr[2-i][j];
+        fArr.arr[2-i][j] = temp[i];
     }
 }
 
 void RPlus() {
     int j = 2;
     char temp[3];
+    rArr.right();
     for(int i = 0; i<3; ++i){
-        temp[i] = bArr[2-i][j];
-        bArr[2-i][j] = uArr[i][j];
-        uArr[i][j] = fArr[i][j];
-        fArr[i][j] = dArr[i][j];
-        dArr[i][j] = temp[i];
+        temp[i] = bArr.arr[2-i][j];
+        bArr.arr[2-i][j] = uArr.arr[i][j];
+        uArr.arr[i][j] = fArr.arr[i][j];
+        fArr.arr[i][j] = dArr.arr[i][j];
+        dArr.arr[i][j] = temp[i];
     }
 
 }
@@ -81,12 +119,13 @@ void RPlus() {
 void UPlus() {
     int i = 0;
     char temp[3];
+    uArr.right();
     for(int j = 0; j<3; ++j){
-        temp[j] = bArr[i][j];
-        bArr[i][j] = lArr[i][j];
-        lArr[i][j] = fArr[i][j];
-        fArr[i][j] = rArr[i][2-j];
-        rArr[i][2-j] = temp[j];
+        temp[j] = bArr.arr[i][j];
+        bArr.arr[i][j] = lArr.arr[i][j];
+        lArr.arr[i][j] = fArr.arr[i][j];
+        fArr.arr[i][j] = rArr.arr[i][j];
+        rArr.arr[i][j] = temp[j];
     }
 }
 
@@ -94,36 +133,39 @@ void UPlus() {
 void UMinus() {
     int i = 0;
     char temp[3];
+    uArr.left();
     for(int j = 0; j<3; ++j){
-        temp[j] = bArr[i][j];
-        bArr[i][j] = rArr[i][2-j];
-        rArr[i][2-j] = fArr[i][j];
-        fArr[i][j] = lArr[i][j];
-        lArr[i][j] = temp[j];
+        temp[j] = bArr.arr[2-i][j];
+        bArr.arr[i][2-j] = rArr.arr[i][j];
+        rArr.arr[i][j] = fArr.arr[i][j];
+        fArr.arr[i][j] = lArr.arr[i][j];
+        lArr.arr[i][j] = temp[j];
     }
 }
 
 void DPlus() {
     int i = 2;
     char temp[3];
+    dArr.right();
     for(int j = 0; j<3; ++j){
-        temp[j] = bArr[i][j];
-        bArr[i][j] = rArr[i][2-j];
-        rArr[i][2-j] = fArr[i][j];
-        fArr[i][j] = lArr[i][j];
-        lArr[i][j] = temp[j];
+        temp[j] = bArr.arr[2-i][j];
+        bArr.arr[i][2-j] = rArr.arr[i][j];
+        rArr.arr[i][j] = fArr.arr[i][2-j];
+        fArr.arr[i][2-j] = lArr.arr[i][j];
+        lArr.arr[i][j] = temp[j];
     }
 }
 
 void DMinus() {
     int i = 2;
     char temp[3];
+    dArr.left();
     for(int j = 0; j<3; ++j){
-        temp[j] = bArr[i][j];
-        bArr[i][j] = lArr[i][j];
-        lArr[i][j] = fArr[i][j];
-        fArr[i][j] = rArr[i][2-j];
-        rArr[i][2-j] = temp[j];
+        temp[j] = bArr.arr[i][j];
+        bArr.arr[i][j] = lArr.arr[i][2-j];
+        lArr.arr[i][2-j] = fArr.arr[i][j];
+        fArr.arr[i][j] = rArr.arr[i][2-j];
+        rArr.arr[i][2-j] = temp[j];
     }
 }
 
@@ -131,12 +173,13 @@ void FPlus() {
     int i = 0;
     int k = 2;
     char temp[3];
+    fArr.right();
     for(int j = 0; j<3; ++j){
-        temp[j] = uArr[k][j];
-        uArr[k][j] = lArr[k][j];
-        lArr[k][j] = dArr[i][j];
-        dArr[i][j] = rArr[2-j][i];
-        rArr[2-j][i] = temp[j];
+        temp[j] = uArr.arr[k][2-j];
+        uArr.arr[k][2-j] = lArr.arr[k][j];
+        lArr.arr[k][j] = dArr.arr[i][2-j];
+        dArr.arr[i][2-j] = rArr.arr[j][i];
+        rArr.arr[j][i] = temp[j];
     }
 }
 
@@ -144,12 +187,13 @@ void FMinus() {
     int i = 0;
     int k = 2;
     char temp[3];
+    fArr.left();
     for(int j = 0; j<3; ++j){
-        temp[j] = uArr[k][j];
-        uArr[k][j] = rArr[2-j][i];
-        rArr[2-j][i] = dArr[i][j];
-        dArr[i][j] = lArr[k][j];
-        lArr[k][j] = temp[j];
+        temp[j] = uArr.arr[k][2-j];
+        uArr.arr[k][2-j] = rArr.arr[j][i];
+        rArr.arr[j][i] = dArr.arr[i][j];
+        dArr.arr[i][j] = lArr.arr[k][j];
+        lArr.arr[k][j] = temp[j];
     }
 }
 
@@ -157,12 +201,13 @@ void BPlus() {
     int i = 0;
     int k = 2;
     char temp[3];
+    bArr.right();
     for(int j = 0; j<3; ++j){
-        temp[i] = uArr[i][j];
-        uArr[i][j] = rArr[2-j][k];
-        rArr[2-j][k] = dArr[k][j];
-        dArr[k][j] = lArr[i][j];
-        lArr[i][j] = temp[i];
+        temp[i] = uArr.arr[i][2-j];
+        uArr.arr[i][2-j] = rArr.arr[2-j][k];
+        rArr.arr[2-j][k] = dArr.arr[k][j];
+        dArr.arr[k][j] = lArr.arr[i][j];
+        lArr.arr[i][j] = temp[i];
     }
 }
 
@@ -170,12 +215,13 @@ void BMinus() {
     int i = 0;
     int k = 2;
     char temp[3];
+    bArr.left();
     for(int j = 0; j<3; ++j){
-        temp[j] = uArr[i][j];
-        uArr[i][j] = lArr[i][j];
-        lArr[i][j] = dArr[k][j];
-        dArr[k][j] = rArr[2-j][k];
-        rArr[2-j][k] = temp[j];
+        temp[j] = uArr.arr[i][j];
+        uArr.arr[i][j] = lArr.arr[i][2-j];
+        lArr.arr[i][2-j] = dArr.arr[k][2-j];
+        dArr.arr[k][2-j] = rArr.arr[j][k];
+        rArr.arr[j][k] = temp[j];
     }
 }
 
@@ -225,7 +271,7 @@ void solution(){
         for(int i = 0; i<3; ++i)
         {
             for(int j = 0; j<3; ++j)
-                cout << uArr[i][j];
+                cout << uArr.arr[i][j];
             cout << '\n';
         }
 
@@ -233,7 +279,7 @@ void solution(){
         for(int i = 0; i<3; ++i)
         {
             for(int j = 0; j<3; ++j)
-                cout << lArr[i][j];
+                cout << lArr.arr[i][j];
             cout << '\n';
         }
 
@@ -241,7 +287,7 @@ void solution(){
         for(int i = 0; i<3; ++i)
         {
             for(int j = 0; j<3; ++j)
-                cout << rArr[i][j];
+                cout << rArr.arr[i][j];
             cout << '\n';
         }
 
@@ -249,7 +295,7 @@ void solution(){
         for(int i = 0; i<3; ++i)
         {
             for(int j = 0; j<3; ++j)
-                cout << fArr[i][j];
+                cout << fArr.arr[i][j];
             cout << '\n';
         }
 
@@ -257,7 +303,7 @@ void solution(){
         for(int i = 0; i<3; ++i)
         {
             for(int j = 0; j<3; ++j)
-                cout << bArr[i][j];
+                cout << bArr.arr[i][j];
             cout << '\n';
         }
 
@@ -265,7 +311,7 @@ void solution(){
         for(int i = 0; i<3; ++i)
         {
             for(int j = 0; j<3; ++j)
-                cout << dArr[i][j];
+                cout << dArr.arr[i][j];
             cout << '\n';
         }
         cout << endl;
