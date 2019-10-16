0047313b7907ea2b4775b219c25f6d8d11ef759c
김혜진 <sikhye823@naver.com>
2019-10-16 19:15:22 +0900

    tree new code

--- maejin/4주차/tree_16235.cpp
+++ maejin/4주차/tree_16235.cpp
@@ -11,82 +11,53 @@
 #include <algorithm>
 using namespace std;
 
-struct tree {
-    int x;
-    int y;
-    int age = 1;
-};
-
-bool compare(tree a, tree b){
-    
-    if (a.age == b.age)
-        return true;
-    
-    return a.age < b.age;
-}
-
 int N, M, K;
-int map[11][11];
-int A[11][11];
-vector<tree> trees[11][11];
-vector<tree> dead;
+int map[10][10];
+int A[10][10];
+vector<int> trees[10][10];
 
-
-void spring() {
+void springAndSummer() {
+   
+     for (int i = 0; i < N; i++) {
+           for (int j = 0; j < N; j++){
+               
+               if (!trees[i][j].empty()){
     
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            
-            if (!trees[i][j].empty()){
-                sort(trees[i][j].begin(), trees[i][j].end(), compare);
-                int size = trees[i][j].size();
-                int index = -1;
-                for (int k = 0; k < size; k++) {
-                    
-                    if (map[i][j] < trees[i][j][k].age){
-                        index = k;
-                        break;
-                    }
-                    
-                    map[i][j] -= trees[i][j][k].age;
-                    trees[i][j][k].age++;
-                }
-                
-                if (index != -1){
-                    for (int k = 0; k < size-index; k++) {
-                        dead.push_back(trees[i][j][index]);
-                        trees[i][j].erase(trees[i][j].begin()+index);
-                        M--;
-                    }
-                }
-            }
-        }
-    }
+                   int size = trees[i][j].size();
+                   int dead = size;
+                   sort(trees[i][j].begin(), trees[i][j].end());
+                   
+                   for (int k = 0; k < size; k++){
+                       
+                       if (map[i][j] < trees[i][j][k]){
+                           dead = k;
+                           break;
+                       }
+                       
+                       map[i][j] -= trees[i][j][k];
+                       trees[i][j][k]++;
+                   }
+                   
+                   for (int k = size-1; k > dead-1; k--){
+                       map[i][j] += trees[i][j][k]/2;
+                       trees[i][j].pop_back();
+                       M--;
+                   }
+               }
+           }
+       }
 }
 
 
-void summer() {
-    
-    while (!dead.empty()) {
-        
-        tree temp = dead[0];
-        dead.erase(dead.begin());
-        map[temp.x][temp.y] += temp.age/2;
-    }
-}
-
-
-void creatNewTree(tree curr)
+void creatNewTree(int x, int y)
 {
-    tree temp;
-    for (int i = curr.x-1; i <= curr.x+1; i++) {
-        for (int j = curr.y-1; j <= curr.y+1; j++) {
-            if (i > 0 && i < N+1 && j > 0 && j < N+1) {
+    for (int i = x-1; i <= x+1; i++) {
+        for (int j = y-1; j <= y+1; j++) {
+            
+            if (i > -1 && i < N && j > -1 && j < N) {
                 
-                if (i != curr.x || j != curr.y){
-                    temp.x = i;
-                    temp.y = j;
-                    trees[i][j].push_back(temp);
+                if (i != x || j != y){
+                    trees[i][j].push_back(1);
                     M++;
                 }
             }
@@ -94,16 +65,16 @@ void creatNewTree(tree curr)
     }
 }
 
+
 void fall() {
-    
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             int size = trees[i][j].size();
             for (int k = 0; k < size; k++) {
-                tree curr = trees[i][j][k];
                 //5의 배수라면
-                if (curr.age%5 == 0)
-                    creatNewTree(curr);
+                if (trees[i][j][k]%5 == 0)
+                    creatNewTree(i, j);
             }
         }
     }
@@ -111,33 +82,31 @@ void fall() {
 
 void winter() {
     
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
             map[i][j]+=A[i][j];
     }
 }
 
 int main() {
-    
-    fill(map[0], map[0]+121, 5);
-    
+
     cin >> N >> M >> K;
     
-    
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++){
             cin >> A[i][j];
+            map[i][j] = 5;
+        }
     }
     
-    for (int i = 1; i <= M; i++){
-        tree temp;
-        cin >> temp.x >> temp.y >> temp.age;
-        trees[temp.x][temp.y].push_back(temp);
+    int x,y,age;
+    for (int i = 0; i < M; i++){
+        cin >> x >> y >> age;
+        trees[x-1][y-1].push_back(age);
     }
     
     while(K--){
-        spring();
-        summer();
+        springAndSummer();
         fall();
         winter();
     }
